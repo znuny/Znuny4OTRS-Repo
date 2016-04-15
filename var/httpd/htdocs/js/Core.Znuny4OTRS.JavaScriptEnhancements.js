ac9288056574e8066d1157dc02996f740466e656
Thorsten Eckel <te@znuny.com>
2016-04-15 17:33:41 +0200

    Added $.unshiftOn function to add event registrations before the framework registrations.

--- var/httpd/htdocs/js/Core.Znuny4OTRS.JavaScriptEnhancements.js
+++ var/httpd/htdocs/js/Core.Znuny4OTRS.JavaScriptEnhancements.js
@@ -7,6 +7,27 @@
 // did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 // --
 
+// taken from: http://stackoverflow.com/a/2641047
+// [name] is the name of the event "click", "mouseover", ..
+// same as you'd pass it to on()
+// [fn] is the handler function
+$.fn.unshiftOn = function(name, fn) {
+    // on as you normally would
+    // don't want to miss out on any jQuery magic
+    this.on(name, fn);
+
+    // Thanks to a comment by @Martin, adding support for
+    // namespaced events too.
+    this.each(function() {
+        var handlers = $._data(this, 'events')[name.split('.')[0]];
+
+        // take out the handler we just inserted from the end
+        var handler = handlers.pop();
+        // move it at the beginning
+        handlers.splice(0, 0, handler);
+    });
+};
+
 Core.AJAX.FunctionCallSynchronous = function (URL, Data, Callback, DataType) {
 
     // store the original state
