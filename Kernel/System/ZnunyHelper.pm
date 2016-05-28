a3fe998b8953f23f2fbaf4cae2848f3a6c9c4496
Thorsten Eckel <te@znuny.com>
2016-05-28 15:15:54 +0200

    Fixed redefine warnings.

--- Kernel/System/ZnunyHelper.pm
+++ Kernel/System/ZnunyHelper.pm
@@ -2934,15 +2934,20 @@ sub _PackageSetupInit {
         'ZZZAuto.pm',
     );
 
-    # reload the ZZZ files (mod_perl workaround)
-    for my $ZZZFile (@ZZZFiles) {
+    # disable redefine warnings in this scope
+    {
+        no warnings 'redefine';
 
-        PREFIX:
-        for my $Prefix (@INC) {
-            my $File = $Prefix . '/Kernel/Config/Files/' . $ZZZFile;
-            next PREFIX if !-f $File;
-            do $File;
-            last PREFIX;
+        # reload the ZZZ files (mod_perl workaround)
+        for my $ZZZFile (@ZZZFiles) {
+
+            PREFIX:
+            for my $Prefix (@INC) {
+                my $File = $Prefix . '/Kernel/Config/Files/' . $ZZZFile;
+                next PREFIX if !-f $File;
+                do $File;
+                last PREFIX;
+            }
         }
     }
 
