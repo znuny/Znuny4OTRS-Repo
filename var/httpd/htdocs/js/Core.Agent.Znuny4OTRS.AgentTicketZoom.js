4d9fcad00af9ae22acfc4f98338e8a1ee7977375
Denny Bresch <db@znuny.com>
2016-09-28 13:22:57 +0200

    Added Core.Agent.Znuny4OTRS.AgentTicketZoom.js

---
+++ var/httpd/htdocs/js/Core.Agent.Znuny4OTRS.AgentTicketZoom.js
@@ -0,0 +1,26 @@
+// --
+// Copyright (C) 2012-2016 Znuny GmbH, http://znuny.com/
+// --
+// This software comes with ABSOLUTELY NO WARRANTY. For details, see
+// the enclosed file COPYING for license information (AGPL). If you
+// did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+// --
+
+"use strict";
+
+var Core = Core || {};
+
+/**
+ * @namespace
+ * @exports TargetNS as Core.Agent.Znuny4OTRS.AgentTicketZoom
+ * @description
+ *      This namespace contains the special module functions for AgentTicketZoom.
+ */
+
+Core.Agent.Znuny4OTRS.AgentTicketZoom = (function (TargetNS) {
+
+
+    return TargetNS;
+
+}(Core.Agent.Znuny4OTRS.AgentTicketZoom || {}));
+
