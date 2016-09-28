6237ee088122d27e1c6b73d2ff9d5e97e921eacd
Denny Bresch <db@znuny.com>
2016-09-28 14:33:46 +0200

    Added AddArticleMenu to Core.Agent.Znuny4OTRS.AgentTicketZoom.js.

--- var/httpd/htdocs/js/Core.Agent.Znuny4OTRS.AgentTicketZoom.js
+++ var/httpd/htdocs/js/Core.Agent.Znuny4OTRS.AgentTicketZoom.js
@@ -21,6 +21,72 @@ Core.Agent.Znuny4OTRS = Core.Agent.Znuny4OTRS || {};
 
 Core.Agent.Znuny4OTRS.AgentTicketZoom = (function (TargetNS) {
 
+    /*
+
+    This function adds a Function / Button to ArticleMenu.
+
+    Core.Agent.Znuny4OTRS.AgentTicketZoom.AddArticleMenu({
+        ID:       'NewFunction',
+        Action:   'AgentTicketNote',
+        TicketID: 237,
+        Title:    'TranslateTitle',
+        Text:     'This function is epic',
+        Position: 'append'                   # optional -  'append' | 'prepend' | 5 | 3
+    });
+
+    */
+
+    TargetNS.AddArticleMenu = function (Param) {
+
+        var ParamCheckSuccess = Core.Znuny4OTRSApp.ParamCheck(Param, ['ID', 'Action', 'TicketID', 'Text', 'Title']);
+        if (!ParamCheckSuccess) return;
+
+        // check if at least one article exists
+        if ($('#ArticleItems div a').length == 0) return;
+
+        // update call when another item is selected
+        Core.App.Subscribe('Event.AJAX.ContentUpdate.Callback', function() {
+            TargetNS.AddArticleMenu(Param);
+        });
+
+        // return if new article menu item always exists
+        if ($('#' + Param['ID']).length != 0){
+            return;
+        }
+
+        var ArticleID = $('#ArticleItems div a').attr('name').replace('Article', '');
+
+        var URL = Core.Znuny4OTRSApp.URL({
+            Action:    Param['Action'],
+            TicketID:  Param['TicketID'],
+            ArticleID: ArticleID
+        });
+
+        var Link = $('<a/>', {
+            text:  Param['Text'],
+            id:    Param['ID'],
+            href:  URL,
+            class: 'AsPopup PopupType_TicketAction',
+            title: Param['Title']
+        });
+
+        var ListItem = $('<li/>').append(Link);
+
+        // possibly needed in the future
+        // var numberRegex = /^(\d+)?$/;
+        // if(Param['Position'] && numberRegex.test(Param['Position'])) {
+        //     $('#ArticleItems > div:nth-child(1) > div > div.LightRow.Bottom > ul > li:nth-child(' + Param['Position'] + ')').prepend(ListItem);
+        // }
+
+        if (Param['Position'] && Param['Position'] == 'prepend'){
+            $('#ArticleItems div div ul.Actions').prepend(ListItem);
+        }
+        else{
+            $('#ArticleItems div div ul.Actions').append(ListItem);
+        }
+
+        return ListItem;
+    };
 
     return TargetNS;
 
