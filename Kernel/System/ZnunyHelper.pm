b2b51c33d92c5ea3d9265f9cc096973f4ca5ba5f
Denny Bresch <db@znuny.com>
2017-03-24 08:58:24 +0100

    Tieded.

--- Kernel/System/ZnunyHelper.pm
+++ Kernel/System/ZnunyHelper.pm
@@ -4200,7 +4200,7 @@ sub _ProcessCreateIfNotExists {
     }
     else {
         $Processes = $Self->_ProcessesGet(
-            SubDir => $Param{SubDir}, # SubDir can be undef
+            SubDir => $Param{SubDir},    # SubDir can be undef
         );
     }
 
