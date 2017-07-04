33bc2f2b67a64e025a6f359f9f20a0cba0488c6a
Jens Pfeifer <jens.pfeifer@znuny.com>
2017-07-04 13:11:45 +0000

    Added option to only disable Znuny repositories in package manager.

--- Kernel/Output/HTML/OutputFilterZnuny4OTRSRepoAdminPackageManager.pm
+++ Kernel/Output/HTML/OutputFilterZnuny4OTRSRepoAdminPackageManager.pm
@@ -14,6 +14,7 @@ use warnings;
 our @ObjectDependencies = (
     'Kernel::Config',
     'Kernel::Output::HTML::Layout',
+    'Kernel::System::AuthSession',
 );
 
 sub new {
@@ -28,13 +29,54 @@ sub new {
 sub Run {
     my ( $Self, %Param ) = @_;
 
-    my $LayoutObject = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
-    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $LayoutObject  = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $ConfigObject  = $Kernel::OM->Get('Kernel::Config');
+    my $SessionObject = $Kernel::OM->Get('Kernel::System::AuthSession');
 
     return if $LayoutObject->{Action} ne 'AdminPackageManager';
 
-    my $ReposDisabled = $ConfigObject->Get('Znuny4OTRSRepoDisable');
-    return if !$ReposDisabled;
+    my $RepoDisabled = $ConfigObject->Get('Znuny4OTRSRepoDisable') // 0;
+    my $RemoveOnlineRepository = $RepoDisabled == 1 ? 1 : 0;
+
+    # If only Znuny repositories are disabled, remove online repository
+    # if it previously has been generated with a Znuny URL
+    if ( !$RemoveOnlineRepository && $RepoDisabled == 2 ) {
+        my $SessionID   = $LayoutObject->{SessionID};
+        my %SessionData = $SessionObject->GetSessionIDData(
+            SessionID => $SessionID,
+        );
+        if (
+            %SessionData
+            && $SessionData{UserRepository}
+            && $SessionData{UserRepository} =~ m{znuny}i
+            )
+        {
+            $RemoveOnlineRepository = 1;
+        }
+    }
+
+    if ($RemoveOnlineRepository) {
+        my $OnlineRepositoryTranslatedString = $LayoutObject->{LanguageObject}->Translate('Online Repository');
+        ${ $Param{Data} } =~ s{
+            <div\s*class="WidgetSimple">
+            \s*
+            <div\s*class="Header">
+            \s*
+            <h2>$OnlineRepositoryTranslatedString</h2>
+            .*?
+            </tbody>
+            \s*
+            </table>
+            \s*
+            </div>
+            \s*
+            </div>
+            \s*
+            <br/>
+        }{}smx;
+    }
+
+    return 1 if $RepoDisabled != 1;
 
     # Remove "Update repository information"
     my $UpdateRepositoryTranslatedString = $LayoutObject->{LanguageObject}->Translate('Update repository information');
@@ -42,26 +84,6 @@ sub Run {
 
     # Remove separator
     ${ $Param{Data} } =~ s{(<ul class="ActionList">\s*<li) class="Separated"(>\s*<form)}{$1$2}sm;
-
-    # Remove "online repository"
-    my $OnlineRepositoryTranslatedString = $LayoutObject->{LanguageObject}->Translate('Online Repository');
-    ${ $Param{Data} } =~ s{
-        <div\s*class="WidgetSimple">
-        \s*
-        <div\s*class="Header">
-        \s*
-        <h2>$OnlineRepositoryTranslatedString</h2>
-        .*?
-        </tbody>
-        \s*
-        </table>
-        \s*
-        </div>
-        \s*
-        </div>
-        \s*
-        <br/>
-    }{}smx;
 
     return 1;
 }
