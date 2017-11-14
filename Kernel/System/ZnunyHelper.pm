e2f35d41b1d1574475f509bee34f850900c4a77c
Thorsten Eckel <te@znuny.com>
2017-11-14 17:49:41 +0100

    Fixed bug: XMLData doesn't need a value.

--- Kernel/System/ZnunyHelper.pm
+++ Kernel/System/ZnunyHelper.pm
@@ -3560,7 +3560,7 @@ sub _ITSMVersionAdd {
         );
         return;
     }
-    if ( $Param{XMLData} && !IsHashRefWithData( $Param{XMLData} ) ) {
+    if ( $Param{XMLData} && ref $Param{XMLData} ne 'HASH' ) {
         $Kernel::OM->Get('Kernel::System::Log')->Log(
             Priority => 'error',
             Message  => "Parameter 'XMLData' as hash ref needed!",
