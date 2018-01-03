74ca352e4609af36802294e5b4bd3adc3924b80a
Denny Bresch <db@znuny.com>
2018-01-03 08:59:26 +0100

    Header update.

--- Custom/Kernel/System/UnitTest/Selenium.pm
+++ Custom/Kernel/System/UnitTest/Selenium.pm
@@ -1,6 +1,6 @@
 # --
-# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
-# Copyright (C) 2012-2017 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2001-2018 OTRS AG, http://otrs.com/
+# Copyright (C) 2012-2018 Znuny GmbH, http://znuny.com/
 # --
 # $origin: otrs - cab4d630be0ff58f47cf2dfa2281020ad23cd0db - Kernel/System/UnitTest/Selenium.pm
 # --
