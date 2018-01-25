a646179053101c6bdaa460265e9497498e7be927
Jens Pfeifer <jens.pfeifer@znuny.com>
2018-01-25 07:45:11 +0000

    Fixed return value of ZnunyHelper::_ProcessWidgetDynamicFieldGroupsGet().

--- Kernel/System/ZnunyHelper.pm
+++ Kernel/System/ZnunyHelper.pm
@@ -4606,7 +4606,7 @@ sub _ProcessWidgetDynamicFieldGroupsGet {
     my $AgentTicketZoomConfig           = $ConfigObject->Get('Ticket::Frontend::AgentTicketZoom');
     my %ProcessWidgetDynamicFieldGroups = %{ $AgentTicketZoomConfig->{ProcessWidgetDynamicFieldGroups} };
 
-    return {} if !%ProcessWidgetDynamicFieldGroups;
+    return if !%ProcessWidgetDynamicFieldGroups;
 
     my %Config;
     for my $Group ( sort keys %ProcessWidgetDynamicFieldGroups ) {
