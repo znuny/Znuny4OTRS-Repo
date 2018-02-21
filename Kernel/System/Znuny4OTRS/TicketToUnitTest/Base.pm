393256870a2aedb50b3906a147935c4671e16ab0
Denny Bresch <db@znuny.com>
2018-02-21 10:39:47 +0100

    Added new function 'TicketToUnitTest'.

---
+++ Kernel/System/Znuny4OTRS/TicketToUnitTest/Base.pm
@@ -0,0 +1,28 @@
+# --
+# Copyright (C) 2012-2018 Znuny GmbH, http://znuny.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::Znuny4OTRS::TicketToUnitTest::Base;
+
+use strict;
+use warnings;
+
+our @ObjectDependencies = ();
+
+use Kernel::System::VariableCheck qw(:all);
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+1;
