b747895d71cd68015f929a41b6faeb355e7eca83
Denny Bresch <db@znuny.com>
2018-05-09 16:58:05 +0200

    Applied Codepolicy.

--- Custom/Kernel/System/Package.pm
+++ Custom/Kernel/System/Package.pm
@@ -9,6 +9,7 @@
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 ## nofilter(TidyAll::Plugin::OTRS::Perl::ObjectDependencies)
+## nofilter(TidyAll::Plugin::OTRS::Znuny4OTRS::STDERRCheck)
 
 package Kernel::System::Package;
 
