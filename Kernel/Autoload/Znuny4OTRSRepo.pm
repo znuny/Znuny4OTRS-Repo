592821f8da386b57d0ad77fc79e10a7deae14f5e
Denny Korsukéwitz <db@znuny.com>
2020-01-10 15:11:05 +0100

    Added AdminZnuny4OTRSFiles - File Overview for custom-, changed-, package-files in Admin view.

--- Kernel/Autoload/Znuny4OTRSRepo.pm
+++ Kernel/Autoload/Znuny4OTRSRepo.pm
@@ -1,6 +1,6 @@
 # --
-# Copyright (C) 2001-2019 OTRS AG, https://otrs.com/
-# Copyright (C) 2012-2019 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
+# Copyright (C) 2012-2020 Znuny GmbH, http://znuny.com/
 # --
 # $origin: otrs - 6114661c44c9ca9dec45364b54bfab036ce6e34e - Kernel/System/CloudService/Backend/Run.pm
 # --
