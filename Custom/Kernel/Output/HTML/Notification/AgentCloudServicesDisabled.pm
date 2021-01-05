14dc98027293c09b01f9d40982252c1cfbd0fecc
Denny Korsukéwitz <dk@znuny.com>
2021-01-05 11:52:18 +0100

    Happy new year 2021.

--- Custom/Kernel/Output/HTML/Notification/AgentCloudServicesDisabled.pm
+++ Custom/Kernel/Output/HTML/Notification/AgentCloudServicesDisabled.pm
@@ -1,6 +1,6 @@
 # --
-# Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2012-2020 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
+# Copyright (C) 2012-2021 Znuny GmbH, http://znuny.com/
 # --
 # $origin: otrs - 87629f00b8a02498bf28c802419865b3286ead2e - Kernel/Output/HTML/Notification/AgentCloudServicesDisabled.pm
 # --
